b7472128cc917d5e06783ed7d90f4fc0ce4ba253
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2025-12-01 12:54:17 +0500

    feat: create complete Login UI with error handling

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Expense from './pages/Dashboard/Expense'
+import Income from './pages/Dashboard/Income'
 
 const App = () => {
   return (
-    <div className='text-2xl'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+const Root = () => {
+  // check if user is authenticated/ token exists in local storage
+  const isAuthenticated = !!localStorage.getItem('token');
+  // redirect based on authentication status
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+
+}
